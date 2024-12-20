80ac95276367ce85edc4c1a7c096ec45a13ff7ba
Olajide Olaniyan <jidd32@gmail.com>
2024-12-20 21:11:28 +0000

    correct config error

--- utils.js
+++ utils.js
@@ -1,10 +1,23 @@
 const redis = require('redis');
 
-const redisClient = redis.createClient({ url: 'redis://localhost:6379' });
+const redisPassword = process.env.REDIS_PASSWORD;
+const redisHost = 'redis-19735.c1.us-central1-2.gce.redns.redis-cloud.com';
+const redisPort = 19735;
+
+const redisClient = redis.createClient({
+  url: `redis://:${redisPassword}@${redisHost}:${redisPort}`
+});
+
+
+
 
 const redisConnect = async () => {
-  await redisClient.connect();
-  return redisClient
+  try{
+    await redisClient.connect();
+    return redisClient
+  }catch(err){
+    console.error('Error connecting to Redis Cloud:', err);
+  }
 }
 
 module.exports = redisConnect
