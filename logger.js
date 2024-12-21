17e1b1c054cae8867f7b7edc4cdd595396519952
Olajide Olaniyan <jidd32@gmail.com>
2024-12-21 19:47:05 +0000

    add ekb to server

--- logger.js
+++ logger.js
@@ -1,5 +1,7 @@
 const pino = require('pino');
 
-const logger = pino({ level: process.env.LOG_LEVEL });
+const logger = pino({ 
+  level: process.env.LOG_LEVEL,
+ },);
 
 module.exports = logger;
--- server.js
+++ server.js
@@ -21,6 +21,8 @@ const loggerHTTP =  pinoHttp({
   }, 
 })
 
+
+
 const schema = buildSchema(`
   type Query {
     AllUsers: [User]
